f80e4df07b6a6ebe14955a02ee1bfe8c80fceb8b
nikos.sotiropoulos <nikolaos.sotiropoulos@commercetools.de>
2018-09-27 12:52:16 +0200

    WIP: ExampleBundle tests

--- Tests/StateBundleTest.php
+++ Tests/StateBundleTest.php
@@ -6,16 +6,24 @@
 namespace Commercetools\Symfony\StateBundle\Tests;
 
 
+use Commercetools\Symfony\CtpBundle\DependencyInjection\Compiler\ProfilerControllerPass;
 use Commercetools\Symfony\StateBundle\DependencyInjection\StateExtension;
 use Commercetools\Symfony\StateBundle\StateBundle;
 use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 
 class StateBundleTest extends TestCase
 {
-    public function testGetContainerExtension()
+    public function testBuild()
     {
+        $container = $this->prophesize(ContainerBuilder::class);
+        $container->addCompilerPass(Argument::type(ProfilerControllerPass::class))->shouldBeCalledOnce();
+
         $stateBundle = new StateBundle();
+
         $this->assertInstanceOf(StateExtension::class, $stateBundle->getContainerExtension());
+        $stateBundle->build($container->reveal());
     }
 }
